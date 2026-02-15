f2bb20eab91f2cd03033d594a85daf28748bbc76
RiderDEAD <98644205+RiderDEAD@users.noreply.github.com>
2026-02-16 04:19:12 +0500

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,16 @@
+@echo off
+D:
+cd "D:\PROEKT MKM\Kaspi oke"
+
+:: 1. Красим всё в хакерский зеленый
+color a
+
+echo Running . . ....
+
+:: 2. Запуск сервера в фоне
+start /b python -m http.server 8000
+
+echo app was runned. . .
+
+pause  Y;'.
++'
